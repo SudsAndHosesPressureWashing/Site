109cb620d61ee207340d99e233600049dc63d9ad
David Smit <davidsmit@Davids-MacBook-Air.local>
2024-03-02 23:24:31 -0500

    Removing the prerender

--- svelte.config.js
+++ svelte.config.js
@@ -14,12 +14,12 @@ const config = {
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter()
 	},
-	prerender: {
-		crawl: true,
-		enabled: true,
-		force: true,
-		pages: ['*']
-	}
+	// prerender: {
+	// 	crawl: true,
+	// 	enabled: true,
+	// 	force: true,
+	// 	pages: ['*']
+	// }
 };
 
 export default config;
