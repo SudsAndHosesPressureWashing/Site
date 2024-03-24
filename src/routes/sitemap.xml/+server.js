50cd3794158050ddd2fea96d575a85e03d4f2b0f
David Smit <davidsmit@Davids-MacBook-Air.local>
2024-03-23 23:32:32 -0400

    Added sitemap

--- src/routes/sitemap.xml/+server.js
+++ src/routes/sitemap.xml/+server.js
@@ -10,7 +10,36 @@ export async function GET() {
 			xmlns:image="https://www.google.com/schemas/sitemap-image/1.1"
 			xmlns:video="https://www.google.com/schemas/sitemap-video/1.1"
 		>
-			<!-- <url> elements go here -->
+			<url>
+                <loc>https://sudsandhosespressurewashing.com</loc>
+            </url>
+            <url>
+                <loc>https://sudsandhosespressurewashing.com/getaquote</loc>
+            </url>
+            <url>
+                <loc>https://sudsandhosespressurewashing.com/services</loc>
+            </url>
+            <url>
+                <loc>https://sudsandhosespressurewashing.com/services/decks-and-porches</loc>
+            </url>
+            <url>
+                <loc>https://sudsandhosespressurewashing.com/services/driveways</loc>
+            </url>
+            <url>
+                <loc>https://sudsandhosespressurewashing.com/services/House-Washing-</loc>
+            </url>
+            <url>
+                <loc>https://sudsandhosespressurewashing.com/services/sidewalks</loc>
+            </url>
+            <url>
+                <loc>https://sudsandhosespressurewashing.com/services/roofs</loc>
+            </url>
+            <url>
+                <loc>https://sudsandhosespressurewashing.com/services/gutters</loc>
+            </url>
+            <url>
+                <loc>https://sudsandhosespressurewashing.com/services/projects</loc>
+            </url>
 		</urlset>`.trim(),
 		{
 			headers: {
