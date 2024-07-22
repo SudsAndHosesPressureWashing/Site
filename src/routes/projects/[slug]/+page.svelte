5e7dd573eea9923e37512e45d8334d8508b29678
David Smit <davidsmit@Davids-MacBook-Air.local>
2024-07-21 23:37:56 -0400

    Fixed projects page

--- src/routes/projects/[slug]/+page.svelte
+++ src/routes/projects/[slug]/+page.svelte
@@ -1,9 +1,16 @@
 <script>
 	/** @type {import('./$types').PageData} */
 	import { PortableText } from '@portabletext/svelte';
+	import { urlFor } from '$lib/utils/image';
 	import BeforeAfterImage from '../../../components/BeforeAfterImage.svelte';
 	import ImageCluster from '../../../components/ImageCluster.svelte';
 	export let data;
+	let section = data.project.section || []
+	let sections = data.project.sections || []
+	let images = section.filter(item => item._type == 'image');
+	let otherTypes = sections.filter(item => item != null && item._type != 'image')
+	let allSections = images.concat(otherTypes)
+	
 </script>
 
 <style>
@@ -23,6 +30,12 @@
 		justify-content: center;
 	}
 
+	img {
+		margin-inline: auto;
+		margin-block: 1rem;
+		display: block;
+	}
+
 </style>
 
 <svelte:head>
@@ -36,16 +49,20 @@
 	<BeforeAfterImage 
 		beforeandafterimage={data.project.mainBeforeAndAfter}
 	/>
-	{#each data.project.sections as section} 
-		{#if section._type == "blockdocument"}
-			
-			<PortableText onMissingComponent={false} value={section.longDescription} />
+	{#if sections}
+		{#each allSections as section} 
+			{#if section._type == "blockdocument"}
+				
+				<PortableText onMissingComponent={false} value={section.longDescription} />
 
-		{:else if section._type=="beforeandafterimage"}
-			<BeforeAfterImage beforeandafterimage={section} />
-		{:else if section._type=="imagecluster"}
-			<ImageCluster imagecluster={section}/>
-		{/if}
+			{:else if section._type=="beforeandafterimage"}
+				<BeforeAfterImage beforeandafterimage={section} />
+			{:else if section._type=="imagecluster"}
+				<ImageCluster imagecluster={section}/>
+			{:else if section._type=="image"}
+				<img alt="{section.asset._ref}" src={urlFor(section).width(500).fit("crop").url()} />
+			{/if}
 
-	{/each} 
+		{/each} 
+	{/if}
 </section>
